d77403ecc9a02bd8fdc30298456595220400b521
huongnguyen071723 <huongnguyen071723@gmail.com>
2017-06-15 14:22:52 +0700

    Create page-nguyen-huong.php

--- page-nguyen-huong.php
+++ page-nguyen-huong.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<--<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
@@ -20,4 +20,178 @@
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
 		<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js" type="text/javascript"></script>
 	</body>
+</html>-->
+
+<!--start my code--> 
+<!DOCTYPE html>
+<html>
+<head>
+<title>Huong Nguyen</title>
+
+<meta name="viewport" content="width=device-width"/>
+<meta name="description" content="huong nguyen"/>
+<meta charset="UTF-8"> 
+
+<link type="text/css" rel="stylesheet" href="style.css">
+<link href='http://fonts.googleapis.com/css?family=Rokkitt:400,700|Lato:400,300' rel='stylesheet' type='text/css'>
+
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body <!--id="top"-->>
+<div id="cv" class="instaFade">
+	<div class="mainDetails">
+		<div id="headshot" class="quickFade">
+			<img src="Photo.jpg" alt="Alan Smith" />
+		</div>
+		
+		<div id="name">
+			<h1 class="quickFade delayTwo">Huong Nguyen</h1>
+			<h2 class="quickFade delayThree">DEV Team</h2>
+			
+		</div>
+		<div id="contactDetails" class="quickFade delayFour">
+			<ul>
+				<li>Email: <a href="mailto:joe@bloggs.com" target="_blank">  huongnguyen071723@gmail.com</a></li>
+				<li>Site: <a href="http://www.bloggs.com">http://trainee.magica.top/nguyen-huong/</a></li>
+				<li>Phone: 01234567890</li>
+				<li>Addr: Dormitory of VNU-HCM
+			</ul>
+		</div>
+		<div class="clear"></div>
+	</div>
+	
+	<div id="mainArea" class="quickFade delayFive">
+		<section>
+			<article>
+				<div class="sectionTitle">
+					<h1>About Me</h1>
+				</div>
+				
+				<div class="sectionContent">
+					<p>My full name is Nguyen Huong. I’m from Quang Ngai pronvince. Now
+I’m a student at Ho Chi Minh City University of Technology. My major is
+Computer Engineering.</p>
+				</div>
+			</article>
+			<div class="clear"></div>
+		</section>
+		
+		
+		<section>
+			<div class="sectionTitle">
+				<h1>Experience</h1>
+			</div>
+			
+			<div class="sectionContent">
+				<article>
+					<h2>Magic Company</h2>
+					<p class="subDetails">June 12, 2017 - Present</p>
+					<p>I’m a trainee in Magic Company.</p>
+				</article>
+				
+				<!--article>
+					<h2>Job Title at Company</h2>
+					<p class="subDetails">Janruary 2007 - March 2011</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies massa et erat luctus hendrerit. Curabitur non consequat enim. Vestibulum bibendum mattis dignissim. Proin id sapien quis libero interdum porttitor.</p>
+				</article-->
+				
+				<!--article>
+					<h2>Job Title at Company</h2>
+					<p class="subDetails">October 2004 - December 2006</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies massa et erat luctus hendrerit. Curabitur non consequat enim. Vestibulum bibendum mattis dignissim. Proin id sapien quis libero interdum porttitor.</p>
+				</article-->
+			</div>
+			<div class="clear"></div>
+		</section>
+		
+		
+		<section>
+			<div class="sectionTitle">
+				<h1>Skills</h1>
+			</div>
+				<!--ul class="Skills">
+					<li></li>
+					<li>A Key Skill</li>
+					<li>A Key Skill</li>
+					<li>A Key Skill</li>
+					<li>A Key Skill</li>
+					<li>A Key Skill</li>
+					<li>A Key Skill</li>
+					<li>A Key Skill</li>
+				</ul-->
+				<div class="sectionContent">
+					<article>
+						<p class="Skills"></p>
+						<h2>Computer Skills</h2>
+						<li>Microsoft Word</li> 
+						<li>Microsoft Excel</li> 
+						<li>Power Point</li>
+						<li>Microsoft Windows 7, 8, 10 </li>
+						<p class="Skills"></p>
+						<h2>Soft Skills</h2>
+						<li>Presetation Skill </li>
+						<li>Teamwork</li>
+						<li>Careful</li>
+						<li>Reliability</li>
+						<p class="Skills"></p>
+						<h2>Programming Language</h2>
+						<li>C</li>
+						<li>C++</li>
+					</article>
+				</div>
+			<div class="clear"></div>
+		</section>
+		
+		
+		<section>
+			<div class="sectionTitle">
+				<h1>Education</h1>
+			</div>
+			
+			<div class="sectionContent">
+				<article>
+					<h2>2014 - present, Ho Chi Minh City</h2>
+					<!--p class="subDetails">Qualification</p-->
+					<p>I study at HCMUT.</p>
+				</article>
+				
+				<article>
+					<h2>2011 - 2014, Quang Ngai Province</h2>
+					<!--p class="subDetails">Qualification</p-->
+					<p>I’m a high school’s student in my province.</p>
+				</article>
+				
+				<article>
+					<h2>2002 - 2011, Quang Ngai Province</h2>
+					<!--p class="subDetails">Qualification</p-->
+					<p>I’m a secondary school’s student in my province.</p>
+				</article>
+			</div>
+			<div class="clear"></div>
+		</section>
+		<section>
+			<div class="sectionTitle">
+				<h1>Social Activist</h1>
+			</div>
+				<div class="Skills">
+						<li> Summer Volunteer Campaign</li> 
+						<li> Volunteers for Parasport</li> 
+				</div>
+			<div class="clear"></div>
+		</section>
+	</div>
+</div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-3753241-1");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+</body>
 </html>
+<!-- end my code-->
