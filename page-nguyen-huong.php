ed3dcd8b668a848a4594ca4cbaf247effb857019
huongnguyen071723 <huongnguyen071723@gmail.com>
2017-06-15 14:57:39 +0700

    Create page-nguyen-huong.php

--- page-nguyen-huong.php
+++ page-nguyen-huong.php
@@ -143,8 +143,8 @@ pageTracker._trackPageview();
 		<!-- insert code here end -->
 		
 		
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
-		<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js" type="text/javascript"></script>
+		<!--script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js" type="text/javascript"></script-->
 	</body>
 </html>
 
